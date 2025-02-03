808c4f097f8f2c8984e77901daaa4ee0fa15c211
bhclira <brenohencastro@gmail.com>
2025-02-03 17:06:54 -0300

    Aula 4 : correcoes

---
+++ script.js
@@ -0,0 +1,36 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlist');
+
+//consumir api criada
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+        .catch((error) => console.log(error));
+
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+        
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+//evento manipulação
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+    }
+})
